3d90e2028c186fa328955891659981f6e2df7cf2
Jeremy D <33931501+Popeyou@users.noreply.github.com>
2018-03-08 15:17:53 +0100

    Update vuereservation.php

--- MCV/vue/vuereservation.php
+++ MCV/vue/vuereservation.php
@@ -1,1 +1,14 @@
+<center>
+	<h2> Liste des reservations enregistres </h2>
+	<br/>
+	<table border = 1>
+		<tr> <td>Numero reservation</td><td>Etat</td> <td>Date</td></tr>
+		<?php
+		foreach ($resultats as $unResultat) {
+			echo "<tr>
+			<td>".$unResultat['numr']."</td>
+			<td>".$unResultat['etat']."</td>
+			<td>".$unResultat['dater']."</td>
+			</tr>";
 
+		}
