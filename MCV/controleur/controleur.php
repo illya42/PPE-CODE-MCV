5420dfa6025ed459ed6465b97a2119f998150777
Jeremy D <33931501+Popeyou@users.noreply.github.com>
2018-03-08 13:27:41 +0100

    Update controleur.php

--- MCV/controleur/controleur.php
+++ MCV/controleur/controleur.php
@@ -1,1 +1,47 @@
-
+<?php
+//SERT A CONTROLER / FAIRE DES VERIFICATIONS
+include("modele/modele.php");
+//	CONNECTION
+	function connectionC()
+	{
+	session_start();
+	//connexion à la BDD
+	$con = connexion();
+	if(isset($_POST['formconnexion']))
+	{
+		$mailconnect=htmlspecialchars($_POST['mailconnect']);
+		$mdpconnect=sha1($_POST['mdpconnect']);
+		if(!empty($mailconnect) AND !empty($mdpconnect))
+		{
+			$requser=$bdd->prepare("SELECT * FROM membres WHERE mail= ? AND mdp = ?");
+			$requser->execute(array($mailconnect, $mdpconnect));
+			$userexist = $requser->rowCount();
+			if($userexist==1)
+			{
+				$userinfo=$requser->fetch();
+				$_SESSION['id']=$userinfo['id'];
+				$_SESSION['pseudo']=$userinfo['pseudo'];
+				$_SESSION['mail']=$userinfo['mail'];
+				header("Location: profil.php?id=".$_SESSION['id']);
+			}
+			else
+			{
+				$erreur= "Mauvais mail ou mot de passe";
+			}
+		}
+		else
+		{
+			$erreur="Tous les champs doivent être complétés !";
+		}
+	}
+	deconnexion($con);
+	}
+// DECONNECTION
+	function deconnection();
+	{ 
+	session_start();
+	$_SESSION=array();
+	session_destroy();
+	header=("Location: modele/modele.php")
+	}
+?>
