c6e2e6a447a073ab3aff59e501a216a1caf1b6fc
illya42 <33931632+illya42@users.noreply.github.com>
2018-03-08 15:13:24 +0100

    Add files via upload

--- MCV/index.php
+++ MCV/index.php
@@ -1,1 +1,33 @@
+<?php
+	include("controleur/controleur.php")
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+		<link href="style.css" rel="stylesheet" type="text/css" media="all" />
+	<title> Roilles : Location et maintenance </title>
+</head>
+<body>
+	<center>
+		<h1> Menu </h1>
+			<a href="index.php?page=1"> Connection
+			</a><br/><br/>
+			<a href="index.php?page=2"> Profil
+			</a><br/><br/>
+			<a href="index.php?page=3"> Inscription
+			</a><br/><br/>
+			<a href="index.php?page=4"> Deconnection
+			</a><br/><br/>
+	</center>
+	<?php
+		if (isset($_GET['page']))
+		{
+			$page = $_GET['page'];
+		}else{
+			$page = 0;
+		}
+		switch ($page) 
+		{
+			case 1:
