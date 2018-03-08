7692a1e0df50f4bb3fc644aba356883818c7f125
Massmas <33931611+Massmas@users.noreply.github.com>
2018-03-08 13:19:43 +0100

    Add files via upload

--- connexion.php
+++ connexion.php
@@ -2,26 +2,24 @@
 function connection()
 {
 	session_start();
-
 	//connexion à la BDD
 	$con = connexion();
-
 	if(isset($_POST['formconnexion']))
 	{
 		$mailconnect=htmlspecialchars($_POST['mailconnect']);
 		$mdpconnect=sha1($_POST['mdpconnect']);
 		if(!empty($mailconnect) AND !empty($mdpconnect))
 		{
-			$requser=$bdd->prepare("SELECT * FROM membres WHERE mail= ? AND mdp = ?");
+			$requser=$bdd->prepare("SELECT * FROM client WHERE mail= ? AND mdpc = ?");
 			$requser->execute(array($mailconnect, $mdpconnect));
 			$userexist = $requser->rowCount();
 			if($userexist==1)
 			{
 				$userinfo=$requser->fetch();
-				$_SESSION['id']=$userinfo['id'];
+				$_SESSION['idc']=$userinfo['id'];
 				$_SESSION['pseudo']=$userinfo['pseudo'];
 				$_SESSION['mail']=$userinfo['mail'];
-				header("Location: profil.php?id=".$_SESSION['id']);
+				header("Location: profil.php?id=".$_SESSION['idc']);
 			}
 			else
 			{
@@ -33,10 +31,8 @@ function connection()
 			$erreur="Tous les champs doivent être complétés !";
 		}
 	}
-
 	deconnexion($con);
 }
-
 ?>
 	<!DOCTYPE html>
 	<html>
