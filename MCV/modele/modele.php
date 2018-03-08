902e24a308c9850993cb5ee2a570c46991a8ee51
illya42 <33931632+illya42@users.noreply.github.com>
2018-03-08 15:14:04 +0100

    Add files via upload

--- MCV/modele/modele.php
+++ MCV/modele/modele.php
@@ -1,11 +1,15 @@
 <?php
 //INJECTE LES REQUETE SQL
+
     function connexion()
     {
     $con = mysqli_connect("localhost","root","","locationppe");
     return $con;
     }
+
     function deconnexion ($con)
     {
         mysqli_close($con);
     }
+
+    
