4c201ab1587ccb8ccdcf7750857bb7e8d510b184
Jeremy D <33931501+Popeyou@users.noreply.github.com>
2018-03-08 15:19:42 +0100

    Update vuetechnicien.php

--- MCV/vue/vuetechnicien.php
+++ MCV/vue/vuetechnicien.php
@@ -1,1 +1,15 @@
+<center>
+	<h2> Liste des cours enregistres </h2>
+	<br/>
+	<table border = 1>
+		<tr> <td>ID technicien</td><td>Nom</td> <td>Prenom</td> <td>mail</td></tr>
+		<?php
+		foreach ($resultats as $unResultat) {
+			echo "<tr>
+			<td>".$unResultat['idt']."</td>
+			<td>".$unResultat['nomt']."</td>
+			<td>".$unResultat['prenomt']."</td>
+			<td>".$unResultat['mailt']."</td>
+			</tr>";
 
+		}
